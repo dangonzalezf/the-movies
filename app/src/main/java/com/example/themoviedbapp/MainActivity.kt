b2c1005258e99f05de1bf9ede63ab47cf3becb1f
dangonzalezf <dangonzalezf@gmail.com>
2024-04-22 20:56:02 -0400

    Compose movie list screen created.

--- app/src/main/java/com/example/themoviedbapp/MainActivity.kt
+++ app/src/main/java/com/example/themoviedbapp/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -15,38 +16,67 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 import com.example.themoviedbapp.ui.theme.TheMovieDBAppTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             TheMovieDBAppTheme {
                 // A surface container using the 'background' color from the theme
+
+                //val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior() // change the toolbar color
+                val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior() // collapse the toolbar
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    MovieGrid()
+                    MovieTopBar(scrollBehavior)
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MovieGrid() {
+fun MovieTopBar(scrollBehavior: TopAppBarScrollBehavior) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "Movies App") },
+                scrollBehavior = scrollBehavior
+            )
+        },
+        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection)
+    ) { padding ->
+        MovieGrid(padding)
+    }
+}
+
+@Composable
+fun MovieGrid(padding: PaddingValues) {
     LazyVerticalGrid(
         columns = GridCells.Adaptive(minSize = 120.dp),
-        modifier = Modifier.padding(4.dp),
         verticalArrangement = Arrangement.spacedBy(4.dp),
-        horizontalArrangement = Arrangement.spacedBy(4.dp)
+        horizontalArrangement = Arrangement.spacedBy(4.dp),
+        contentPadding = padding,
+        modifier = Modifier.padding(4.dp)
     ) {
         items(movies) {
             MovieItem(movie = it)
@@ -57,11 +87,13 @@ fun MovieGrid() {
 @Composable
 fun MovieItem(movie: Movie) {
     Column {
-        Box(
+        AsyncImage(
+            model = movie.poster,
+            contentDescription = movie.title,
             modifier = Modifier
                 .fillMaxWidth()
                 .aspectRatio(2 / 3f)
-                .background(Color.Gray)
+                .clip(MaterialTheme.shapes.small)
         )
         Text(text = movie.title, style = MaterialTheme.typography.bodySmall, maxLines = 1, modifier = Modifier.padding(8.dp))
     }
@@ -77,7 +109,7 @@ val movies = (1..100).map {
     Movie(
         id = it,
         title = "Movie $it",
-        poster = "http://www.movies.cl/id=$it"
+        poster = "https://picsum.photos/200/300?id=$it"
     )
 }
 
@@ -91,7 +123,6 @@ val movies = (1..100).map {
 fun GreetingPreview() {
     TheMovieDBAppTheme {
         Surface {
-            MovieGrid()
         }
     }
 }
