c67f49b6f02aa96aa8c205629fb34409541438f8
dangonzalezf <dangonzalezf@gmail.com>
2024-07-08 08:59:08 -0400

    Se aplico el principio Single Source of Truth  en el repositorio de movies.

--- app/src/main/java/com/example/themoviedbapp/data/MoviesRepository.kt
+++ app/src/main/java/com/example/themoviedbapp/data/MoviesRepository.kt
@@ -3,6 +3,7 @@ package com.example.themoviedbapp.data
 import com.example.themoviedbapp.data.datasource.MoviesLocalDataSource
 import com.example.themoviedbapp.data.datasource.MoviesRemoteDataSource
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.transform
 
 class MoviesRepository(
@@ -11,20 +12,21 @@ class MoviesRepository(
     private val remoteDataSource: MoviesRemoteDataSource
 ) {
 
-    val movies: Flow<List<Movie>> = localDataSource.movies.transform { localMovies ->
-        val movies = localMovies.takeIf { it.isNotEmpty() }
-            ?: remoteDataSource.fetchPopularMovies(regionRepository.findLastRegion()).also {
-            localDataSource.saveMovies(it)
+    val movies: Flow<List<Movie>> = localDataSource.movies.onEach { localMovies ->
+        if (localMovies.isEmpty()) {
+            val region = regionRepository.findLastRegion()
+            val movies = remoteDataSource.fetchPopularMovies(region)
+            localDataSource.saveMovies(movies)
         }
-        emit(movies)
     }
 
-    fun fetchMovieById(id: Int): Flow<Movie> = localDataSource.findMovieById(id).transform { localMovie ->
-        val movie = localMovie ?: remoteDataSource.fetchMovieById(id).also { movie ->
-            localDataSource.saveMovies(listOf(movie))
+    fun fetchMovieById(id: Int): Flow<Movie?> = localDataSource.findMovieById(id).onEach { movie ->
+        if(movie == null) {
+            val remoteMovie = remoteDataSource.fetchMovieById(id)
+            localDataSource.saveMovies(listOf(remoteMovie))
         }
-        emit(movie)
     }
+
 
     suspend fun toggleFavorite(movie: Movie) {
         localDataSource.saveMovies(listOf(movie.copy(favorite = !movie.favorite)))
